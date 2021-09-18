b40925017e33fcae73f99119174af59a7dba4913
Amit Verma <vermamit89@gmail.com>
2021-09-18 19:41:04 +0530

    feat: Palindrome integer

---
+++ 09_palindrome_integer.py
@@ -0,0 +1,16 @@
+x=1000021
+x_str=str(x)
+x_str_rev=x_str[::-1]
+if x_str==x_str_rev:
+    print(True)
+else:
+    print(False)
+
+
+# for i in range(len(x_str)):
+#     if x_str[i]==x_str_rev[i]:
+#         yes=True
+#     else:
+#         yes=False
+# print(yes)
+    
